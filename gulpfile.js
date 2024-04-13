d00fe9cbc46f017d6e4200b4f2d75748e7a31b59
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-04-13 18:08:18 -0300

    scss compiler and image compressor

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('src/styles/*.scss')
-       .pipe(sass())
+       .pipe(sass({outputStyle: 'compressed'}))
        .pipe(gulp.dest('dist/styles'));
 }
+
+function images() {
+    return gulp.src('src/images/**/*')
+       .pipe(imagemin())
+       .pipe(gulp.dest('dist/images'));
+}
+
+exports.default = gulp.parallel(styles,images);
+exports.watch = gulp.parallel(styles);
