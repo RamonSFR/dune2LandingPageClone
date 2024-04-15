1ddf202063f4965bab98bec1859d8165117ca5ac
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-04-15 19:55:55 -0300

    a

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-       .pipe(sass({outputStyle: 'compressed'}))
-       .pipe(gulp.dest('./dist/styles'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/styles'));
 }
 
-exports.default = gulp.parallel(styles);
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+function videos() {
+    return gulp.src('./src/videos/**/*')
+        .pipe(gulp.dest('./dist/videos'));
+}
+
+exports.default = gulp.parallel(styles, images, videos);
 exports.watch = function () {
     gulp.watch('src/styles/*.scss', styles);
 }
