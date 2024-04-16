37af73bad7b6bd64a1eb76fec1ca08a1a82c3612
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-04-16 13:44:32 -0300

    trailers section done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
-function soma(a, b) {
-    return a + b;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const thumbnails = document.querySelectorAll('[data-bs-id]');
 
-console.log(soma(17, 32));
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', function() {
+            thumbnails.forEach(thumbnail => {
+                thumbnail.classList.remove('trailers_video_wrapper_thumbnails_thumbnail--is-active');
+            });
+
+            this.classList.add('trailers_video_wrapper_thumbnails_thumbnail--is-active');
+
+            const videoId = this.getAttribute('data-bs-id');
+
+            const videoElement = document.querySelector('.trailers_video_wrapper_main_video video');
+            videoElement.src = `./dist/videos/${videoId}.mp4`;
+        });
+    });
+});
